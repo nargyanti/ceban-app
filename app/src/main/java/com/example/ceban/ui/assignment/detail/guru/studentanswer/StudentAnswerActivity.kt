7151478bf12598393c0dad3ec207c90c548f504e
Irfan Rafif <mirfanrafif17@gmail.com>
2021-08-09 12:20:13 +0700

    selesai student answer

--- app/src/main/java/com/example/ceban/ui/assignment/detail/guru/studentanswer/StudentAnswerActivity.kt
+++ app/src/main/java/com/example/ceban/ui/assignment/detail/guru/studentanswer/StudentAnswerActivity.kt
@@ -2,9 +2,11 @@ package com.example.ceban.ui.assignment.detail.guru.studentanswer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.ceban.core.model.Assignment
 import com.example.ceban.core.model.Student
 import com.example.ceban.databinding.ActivityStudentAnswerBinding
+import com.synnapps.carouselview.CarouselView
 
 const val EXTRA_STUDENT = "extra_student"
 const val EXTRA_ASSIGNMENT = "extra_assignment"
@@ -13,6 +15,8 @@ class StudentAnswerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityStudentAnswerBinding
     private var student: Student? = null
     private var assignment: Assignment? = null
+
+    var imageList = arrayListOf("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +28,10 @@ class StudentAnswerActivity : AppCompatActivity() {
         super.onStart()
         student = intent.getParcelableExtra(EXTRA_STUDENT)
         assignment = intent.getParcelableExtra(EXTRA_ASSIGNMENT)
+
+        binding.crsStudentAnswer.pageCount = imageList.size
+        binding.crsStudentAnswer.setImageListener { position, imageView ->
+            Glide.with(this).load(imageList[position]).into(imageView)
+        }
     }
 }
